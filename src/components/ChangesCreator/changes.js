fd3ed1111f5b6602461b24d9478f48bf8397c312
muaaz9329 <63157500+muaaz9329@users.noreply.github.com>
2022-08-04 07:50:43 +0500

    added zaino project in portfolio

--- src/components/ChangesCreator/changes.js
+++ src/components/ChangesCreator/changes.js
@@ -5,6 +5,7 @@ import image4 from './skill_imgs/gr4.png'
 import image5 from './skill_imgs/gr5.png'
 import image6 from './skill_imgs/gr6.png'
 import ResBuild from './work_imgs/resBuild.png'
+import Zaino from './work_imgs/Zaino.png'
 export const SkillAdd = [image1,image2,image3,image4,image5,image6]
 export const workPage=[{
     id:0,
@@ -12,5 +13,11 @@ export const workPage=[{
     title:'Resume builder',
     para:'This website was developed by me for users to make their resume by just providing information and choosing their favourite design. The whole website is made with React.js and I am currently working to make it responsive',
     link:'https://resumebuilder-ver1.netlify.app/'},
+    {
+    id:1,
+    image:Zaino,
+    title:'Zaino',
+    para:'This website was developed by me for users to know the weather condition of thier current location, This site uses OpenWeather API and Weather.io API for accurate information , Fully responsive designed and automatically reads the location of The user, With current weather condition this site also provides info for upcoming 4 days as well as Wind speed and Humidity.',
+    link:'https://zaino.netlify.app/'}
     ]
 export const aboutData = ' I am a hardworking and ambitious individual with a great passion for Computer Science and Designing thats why I have more than a year of Experince in React. I am currently in my second semester of studying BS Computer Science at Khawaja Fareed University Of Engineering and Technology. I have good Programming skills, enabling me to effectively work with React and its eco system. My main Passion is making creative and out of box idea into reality.'
--- src/components/workPage/cardGenerator.js
+++ src/components/workPage/cardGenerator.js
@@ -26,7 +26,7 @@ const CardGenerator = (props)=>{
 
   const OddCard = <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center rounded-lg" key={title}>
     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 rounded-2xl block md:hidden" key={image}>
-      <a href={link} target="_blank" rel="noreferrer">  <img className="object-cover object-center  shadow-2xl rounded transition-all hover:scale-110" alt="hero" src={image} /></a>
+      <a href={link} target="_blank" rel="noreferrer">  <img className="object-cover object-center  shadow-2xl rounded transition-all  hover:scale-110" alt="hero" src={image} /></a>
     </div>
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-center md:text-center items-center text-center md:-mb-6" key={para}>
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{title}
@@ -34,7 +34,7 @@ const CardGenerator = (props)=>{
       <p className="mb-8 leading-relaxed">{para}</p>
     </div>
     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 rounded-2xl hidden md:block" key={image}>
-      <a href={link} target="_blank" rel="noreferrer">  <img className="object-cover object-center  shadow-2xl rounded transition-all hover:scale-110" alt="hero" src={image} /></a>
+      <a href={link} target="_blank" rel="noreferrer">  <img className="object-cover object-center  shadow-2xl rounded transition-all duration-700 hover:scale-110" alt="hero" src={image} /></a>
     </div>
   </div>;
 
