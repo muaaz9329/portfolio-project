08a52e7bab5d6e763587cd92b3d49fdfbe6fd7b1
muaaz9329 <63157500+muaaz9329@users.noreply.github.com>
2022-09-01 01:51:25 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <footer class="text-cusWhite bg-cusBlue body-font">
         
   <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-  <a href='https://muaazkhan.netlify.app' target="_blank" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+  <a href='https://muaazkhan.netlify.app'  class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
 
 <span class="ml-3 text-3xl">MK.</span>
 </a>
@@ -16,7 +16,7 @@ function Footer() {
     <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
     
 
-      <a class="ml-3 text-gray-500" href='https://www.linkedin.com/in/muaaz-khan-a8731b232/' target="_blank">
+      <a class="ml-3 text-gray-500" href='https://www.linkedin.com/in/muaaz-khan-a8731b232/' >
       <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
           <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
           <circle cx="4" cy="4" r="2" stroke="none"></circle>
